4f2eb0a31d7c4b83bb5b09be107155648da2cc23
ralpdexterbongato <rdc2898@gmail.com>
2018-04-20 18:59:39 +0800

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,22 +11,17 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-  public function boot(UrlGenerator $url)
-   {
-       if(env('REDIRECT_HTTPS')) {
-           $url->formatScheme('https');
-       }
-   }
-
-   /**
-    * Register any application services.
-    *
-    * @return void
-    */
-   public function register()
-   {
-       if(env('REDIRECT_HTTPS')) {
-           $this->app['request']->server->set('HTTPS', true);
-       }
-   }
+     public function boot()
+     {
+         Schema::defaultStringLength(191);
+     }
+     /**
+      * Register any application services.
+      *
+      * @return void
+      */
+     public function register()
+     {
+         //
+     }
 }
